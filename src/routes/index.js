4355bad53b2f2917a29bd566be834fefb38a930d
user17 <rajpreetgiri4@gmail.com>
2022-03-30 15:35:07 +0530

    otp update

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,17 +7,11 @@ const router = express.Router()
 router.post('/register', register_controller.register)
 router.post('/blog', blog_controller.blog)
 router.post('/otp', otp_verification.otp_register)
+
 //Admin
 
 
 //App
-router.post('/getblog', async (req, res) => {
-    Blog.find({}).populate('user').populate('prompts').exec((err, result) => {
-        if (err) res.json({message: "2 Minutes rukkja bhai error hai"})
-        else res.json(result)
-    })
-
-})
 
 
 export default router
